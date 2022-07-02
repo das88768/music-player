a688144c47144401502004bd0332ba4eb2c54504
Akash Das <das88768@gmail.com>
2022-07-02 18:17:29 +0530

    Add the song time elapsed, total song duration and song position slider

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import audio_metadata
 from PIL import ImageTk, Image
 from io import BytesIO
 import os
+import time
+from mutagen.mp3 import MP3
 
 # Function to play the selected songs from the list or directory.
 def play_song(song_name: StringVar, song_list: Listbox, status: StringVar):
@@ -17,6 +19,14 @@ def play_song(song_name: StringVar, song_list: Listbox, status: StringVar):
     # Load the selected song and start the mixer/play the song.
     mixer.music.load(song_list.get(ACTIVE))
     mixer.music.play()
+
+    # Call the play_time function when the song is played.
+    global duration
+    metadata=audio_metadata.load(song_list.get(ACTIVE))
+    song_len = metadata.streaminfo['duration']
+    duration = time.strftime('%M:%S', time.gmtime(song_len))
+    
+    play_time()
 
     # Set the status of the player to Playing.
     status.set("Song Playing..")
@@ -65,8 +75,24 @@ def volume(x):
     value = volume_slider.get()
     mixer.music.set_volume(value/100)
 
-def get_song_img(sl):
+def slide(x):
+    pass
 
+def play_time():
+    # Fetch the song's current time position,
+    current_time = mixer.music.get_pos() / 1000
+
+    # Convert the time into minute and second format.
+    converted_current_time = time.strftime('%M:%S', time.gmtime(current_time))
+    
+    # show the time on the duration frame.
+    song_len = duration
+    duration_frame.config(text=f"Time Elapsed: {converted_current_time} / {song_len}")
+    
+    # Update the time frame every second.
+    duration_frame.after(1000, play_time)
+
+def get_song_img(sl):
     #get all metadata of song.mp3
     metadata=audio_metadata.load(sl)
     artwork = metadata.pictures[0].data
@@ -84,7 +110,7 @@ mixer.init()
 
 # Initializing the parent window of the GUI and set the resolution and the title of the GUI.
 root = Tk()
-root.geometry('700x220')
+root.geometry('700x270')
 root.title('My Music Player')
 
 # It helps to stop the change of the window size.
@@ -95,7 +121,7 @@ root.resizable(False, False)
 song_frame = LabelFrame(root, text="Current song", bg='LightBlue', width=506, height=80)
 song_frame.place(x=0, y=0)
 
-button_frame = LabelFrame(root, text="Control Buttons", bg='Turquoise', width=506, height=120)
+button_frame = LabelFrame(root, text="Control Buttons", bg='Turquoise', width=506, height=170)
 button_frame.place(y=80)
 
 listbox_frame = LabelFrame(root, text='Playlist', bg="RoyalBlue", height=200, width=300)
@@ -103,6 +129,9 @@ listbox_frame.place(x=505, y=0)
 
 volume_frame = LabelFrame(root, text="Volume", bg="Turquoise")
 volume_frame.place(x=400, y=85)
+
+duration_frame = Label(root, bg='pink', text='Time', bd=2, relief=GROOVE, width=28, height=2)
+duration_frame.place(x=505, y=214)
 
 # StringVar is used to manipulate text in entry, labels.
 current_song = StringVar(root, value='<Not selected>')
@@ -141,9 +170,14 @@ resume_btn.place(x=285, y=10)
 dir_btn = Button(button_frame, text="Load Directory", bg='Aqua', font=("Georgia", 13), width=35, command=lambda: load(playlist))
 dir_btn.place(x=10, y=55)
 
+# Control the volume of the song.
 volume_slider = Scale(volume_frame, from_=100, to=0, orient=VERTICAL, command=volume, length=90, bg='orange', cursor='hand2')
 volume_slider.set(30)
 volume_slider.pack()
+
+# Position slider to move at any position in the music.
+slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, command=slide, length=250, cursor='hand2')
+slider.place(x=120, y=200)
 
 Label(root, textvariable=song_status, bg='SteelBlue', font=('Times', 8), justify=LEFT).pack(side=BOTTOM, fill=X)
 
